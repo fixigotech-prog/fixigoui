116fe4ced171b0041919cdea5ef02466980f21a1
Nikhil Jahagirdar <nikhil.jahagirdar@gmail.com>
2025-09-25 17:18:30 +0530

    m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
+import {getMessages,getLocale} from 'next-intl/server';
 import {ReactNode} from 'react';
 import './globals.css'; // Assuming you have a global CSS file
 
 export default async function RootLayout({
   children,
-  params: {locale}
+  
 }: {
   children: ReactNode;
   params: {locale: string};
 }) {
   // Providing all messages to the client side is the easiest way to get started
+  const locale = await getLocale();
   const messages = await getMessages();
 
   return (
